8835f3007da6e69df9d96449717ab963d5cdcfe4
Andriy Horen <go.andriy@gmail.com>
2020-12-05 02:21:02 +0100

    Add deque tests

--- packages/data-structures/src/deque/deque.test.ts
+++ packages/data-structures/src/deque/deque.test.ts
@@ -238,3 +238,148 @@ describe('Pop method', () => {
 		expect(deq.capacity).toEqual(16384);
 	});
 });
+
+describe('Unshift method', () => {
+	test('it should add elements into a collection', () => {
+		const deq = new Deque();
+		const len1 = deq.unshift(1);
+
+		expect(len1).toEqual(1);
+		expect(deq.length).toEqual(1);
+		expect(deq.capacity).toEqual(16);
+		expect([...deq]).toEqual([1]);
+
+		const len2 = deq.unshift(2);
+
+		expect(len2).toEqual(2);
+		expect(deq.length).toEqual(2);
+		expect(deq.capacity).toEqual(16);
+		expect([...deq]).toEqual([2, 1]);
+
+		const len3 = deq.unshift(3);
+
+		expect(len3).toEqual(3);
+		expect(deq.length).toEqual(3);
+		expect(deq.capacity).toEqual(16);
+		expect([...deq]).toEqual([3, 2, 1]);
+	});
+
+	test('it should add elements into a collection with pre-existing elements', () => {
+		const deq = new Deque([-100, -10, 0]);
+		const len1 = deq.unshift(1);
+
+		expect(len1).toEqual(4);
+		expect(deq.length).toEqual(4);
+		expect(deq.capacity).toEqual(16);
+		expect([...deq]).toEqual([1, -100, -10, 0]);
+
+		const len2 = deq.unshift(2);
+
+		expect(len2).toEqual(5);
+		expect(deq.length).toEqual(5);
+		expect(deq.capacity).toEqual(16);
+		expect([...deq]).toEqual([2, 1, -100, -10, 0]);
+
+		const len3 = deq.unshift(3);
+
+		expect(len3).toEqual(6);
+		expect(deq.length).toEqual(6);
+		expect(deq.capacity).toEqual(16);
+		expect([...deq]).toEqual([3, 2, 1, -100, -10, 0]);
+	});
+
+	test('it should add element when capacity is reached', () => {
+		// prettier-ignore
+		const deq = new Deque([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
+		expect(deq.length).toEqual(deq.capacity);
+		const len = deq.unshift(100);
+
+		expect(len).toEqual(17);
+		expect(deq.length).toEqual(17);
+		expect(deq.capacity).toEqual(32);
+		// prettier-ignore
+		expect([...deq]).toEqual([100,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
+	});
+
+	test('it should add elements when the buffer is rotated <==', () => {
+		// prettier-ignore
+		const deq = new Deque([1,2,3,4,5,6,7,8,9,10,11,12,13,14]);
+		deq.unshift(-1000);
+		deq.unshift(-100);
+
+		expect(deq.length).toEqual(16);
+		expect(deq.capacity).toEqual(16);
+		// prettier-ignore
+		expect([...deq]).toEqual([-100,-1000,1,2,3,4,5,6,7,8,9,10,11,12,13,14]);
+	});
+
+	test('it should add many elements', () => {
+		const deq = new Deque();
+		for (let i = 0; i < 10_000; i++) {
+			deq.unshift(Math.random());
+		}
+
+		expect(deq.length).toEqual(10_000);
+		expect(deq.capacity).toEqual(16384);
+	});
+});
+
+describe('Shift method', () => {
+	test('it should do nothing when collection is empty', () => {
+		const deq = new Deque();
+		const result = deq.shift();
+
+		expect(result).toBeUndefined();
+		expect(deq.length).toEqual(0);
+		expect(deq.capacity).toEqual(16);
+	});
+
+	test('it should remove and return elements from the front', () => {
+		const deq = new Deque([100, -10, -100]);
+
+		const el1 = deq.shift();
+
+		expect(el1).toEqual(100);
+		expect(deq.length).toEqual(2);
+		expect(deq.capacity).toEqual(16);
+		expect([...deq]).toEqual([-10, -100]);
+
+		const el2 = deq.shift();
+
+		expect(el2).toEqual(-10);
+		expect(deq.length).toEqual(1);
+		expect(deq.capacity).toEqual(16);
+		expect([...deq]).toEqual([-100]);
+
+		const el3 = deq.shift();
+
+		expect(el3).toEqual(-100);
+		expect(deq.length).toEqual(0);
+		expect(deq.capacity).toEqual(16);
+		expect([...deq]).toEqual([]);
+	});
+
+	test('it should remove many elements', () => {
+		const deq = new Deque(Array(10_000).fill(0));
+
+		for (let i = 0; i < 5000; i++) {
+			deq.shift();
+		}
+		expect(deq.length).toEqual(5000);
+		expect(deq.capacity).toEqual(16384);
+
+		for (let i = 0; i < 5000; i++) {
+			deq.shift();
+		}
+		expect(deq.length).toEqual(0);
+		expect(deq.capacity).toEqual(16384);
+	});
+});
+
+describe('ToString method', () => {
+	test('should convert collection to a string', () => {
+		const deq = new Deque([100, 10, 0, -9, -99, -999]);
+
+		expect(deq.toString()).toEqual('100,10,0,-9,-99,-999');
+	});
+});
